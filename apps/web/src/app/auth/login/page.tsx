d9202a0ba53c018284c65284b1a2cc7c872479b3
Anuka Hettiarachchi <anuka3382@gmail.com>
2025-07-02 23:32:38 +0530

    login form + home/map

--- apps/web/src/app/auth/login/page.tsx
+++ apps/web/src/app/auth/login/page.tsx
@@ -1,15 +1,12 @@
 "use client"
-import { LoginForm } from "@/components/ui/login-form"
+import { LoginForm } from "@/components/ui/custom/login-form"
+import {Header} from "@/components/ui/custom/header";
 
 export default function LoginPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 font-inter">
             <div className="flex w-full max-w-sm flex-col gap-6">
-                    <div className="w-full max-w-sm h-44 relative">
-                        <div className="w-full max-w-sm h-40 left-0 top-[6px] absolute bg-[#167C94] rounded-md shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border-solid border-black"></div>
-                        <div className="w-full max-w-sm h-8 left-0 top-[119px] absolute text-center justify-center text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-sm font-medium font-inter leading-[48px] [text-shadow:_0px_4px_4px_rgb(0_0_0_/_0.25)]">Faculty of Science, University of Peradeniya</div>
-                        <div className="w-full max-w-sm h-32 left-[1px] top-[12px] absolute text-center justify-center text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-5xl font-extrabold font-inter leading-[48px] [text-shadow:_0px_4px_4px_rgb(0_0_0_/_0.25)]">Industry Day<br/>2025</div>
-                    </div>
+                    <Header></Header>
                     <LoginForm/>
             </div>
         </div>
--- apps/web/src/components/ui/custom/login-form.tsx
+++ apps/web/src/components/ui/custom/login-form.tsx
@@ -14,7 +14,7 @@ export function LoginForm({
                           }: React.ComponentProps<"div">) {
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
-            <Card className=" bg-amber-200 text-black w-full max-w-sm shadow-lg">
+            <Card className=" bg-gray-300 text-black w-full max-w-sm shadow-lg border-black">
                 <CardHeader className="text-center">
                     <CardTitle className="text-xl">Login to your account</CardTitle>
                     <CardDescription className="text-black">
@@ -25,14 +25,14 @@ export function LoginForm({
                     <form>
                         <div className="grid gap-6">
                             <div className="flex flex-col gap-4">
-                                <Button variant="outline" className="w-full" type="button" onClick={() => {
+                                <Button variant="outline" className="w-full border-black" type="button" onClick={() => {
                                     window.location.href = "http://localhost:3001/api/auth/google/login";
                                 }} >
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                        <path
-                                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                                            fill="currentColor"
-                                        />
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 0 48 48" width="48" height="48">
+                                        <path fill="#FBBC05" d="M9.83,24c0-1.52,0.25-2.98,0.7-4.36L2.62,13.6C1.08,16.73,0.21,20.26,0.21,24c0,3.74,0.87,7.26,2.41,10.39l7.9-6.05c-0.45-1.36-0.7-2.82-0.7-4.34z"/>
+                                        <path fill="#EB4335" d="M23.71,10.13c3.31,0,6.3,1.17,8.65,3.09l6.84-6.83C35.04,2.77,29.7,0.53,23.71,0.53c-9.29,0-17.28,5.31-21.09,13.07l7.91,6.04c1.82-5.53,7.02-9.51,13.18-9.51z"/>
+                                        <path fill="#34A853" d="M23.71,37.87c-6.16,0-11.36-3.98-13.18-9.51l-7.91,6.04C6.45,42.16,14.43,47.47,23.71,47.47c5.73,0,11.19-2.03,15.31-5.85l-7.51-5.8c-2.12,1.33-4.78,2.01-7.8,2.01z"/>
+                                        <path fill="#4285F4" d="M46.15,24c0-1.39-0.21-2.88-0.54-4.27H23.71v9.07h12.6c-0.63,3.09-2.35,5.47-4.79,6.99l7.51,5.8C43.34,37.61,46.15,31.65,46.15,24z"/>
                                     </svg>
                                     Login with Google
                                 </Button>
