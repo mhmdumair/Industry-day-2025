54ee751c7b85db8479255fd9c7882adac4dbbb82
Akesh Indudunu <153709228+akeshindudunu@users.noreply.github.com>
2025-07-09 11:15:22 +0530

    functional carousel

--- apps/web/src/app/test/page.tsx
+++ apps/web/src/app/test/page.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "@/components/custom/header";
 import Announcement from "@/components/custom/announcement";
-import Carousel from "@/components/custom/carousel";
+import { CarouselDemo } from "@/components/custom/carousel";
+
 
 
 const page = () => {
     return(
-        <>
-            <div><Header/></div>
-            <div><Announcement/></div>
-            <div><Carousel/></div>
-        </>
+    
+        <div>
+            <CarouselDemo/>
+        </div>
     )
 }
 
