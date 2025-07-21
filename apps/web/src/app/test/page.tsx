d6d801dcca20c264094a993228faf7df38fad3c5
Akesh Indudunu <153709228+akeshindudunu@users.noreply.github.com>
2025-07-21 15:09:29 +0530

    update

--- apps/web/src/app/test/page.tsx
+++ apps/web/src/app/test/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-
+import header from "@/components/custom/home/header"
 
 
 const page = () => {
     return(
     
         <div>
+            <header></header>
             
         </div>
     )
