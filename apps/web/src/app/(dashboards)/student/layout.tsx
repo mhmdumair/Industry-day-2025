7645de0ec83c73c94922af8b3a3846e39c14b0ab
Akesh Indudunu <153709228+akeshindudunu@users.noreply.github.com>
2025-07-26 13:18:36 +0530

    student profile

--- apps/web/src/app/(dashboards)/student/layout.tsx
+++ apps/web/src/app/(dashboards)/student/layout.tsx
@@ -1,9 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import StudentSidebar from "@/components/student/student-sidebar";
-import Navbar from "@/components/common/navbar";
+
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { cookies } from "next/headers";
+import CompanyNavbar from "@/components/company/company-navbar";
 
 
 export default async function StudentLayout({
@@ -19,8 +20,8 @@ export default async function StudentLayout({
   
       <SidebarProvider defaultOpen={defaultOpen}>
         <StudentSidebar />
-        <div className="w-full">
-          <Navbar />
+        <div className="flex flex-col items-center min-h-screen w-full mx-auto p-2 sm:p-4">
+          <CompanyNavbar />
           <div className="px-4">{children}</div>
         </div>
       </SidebarProvider>
--- apps/web/src/app/auth/login/page.tsx
+++ apps/web/src/app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { LoginForm } from "@/components/auth/login-form"
-import {Header} from "@/components/common/header";
+//import {Header} from "@/components/common/header";
 
 export default function LoginPage() {
     return (
