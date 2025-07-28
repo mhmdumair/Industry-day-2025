34e459a402f26521e113ee7381831ba4a22fec1d
Anuka Hettiarachchi <anuka3382@gmail.com>
2025-07-28 19:00:19 +0530

    api-2 merge

--- apps/web/src/app/(dashboards)/company/pre-listed/page.tsx
+++ apps/web/src/app/(dashboards)/company/pre-listed/page.tsx
@@ -1,9 +1,34 @@
+"use client";
 import React from 'react'
+import {Button} from "@/components/ui/button";
+import {Card, CardHeader} from "@/components/ui/card";
 
-const page = () => {
+export default function CompanyFilter() {
     return (
-        <div>Pre Listed Interviews</div>
-    )
-}
+        <div className="mt-3 w-screen mx-auto p-4 flex flex-col items-center justify-center gap-5">
+            {/* Content Area */}
+                <div className="flex justify-center w-11/12 p-4">
+                    <Card className="bg-slate-100 w-11/12 max-w-2xl rounded-lg shadow-md p-6 text-black space-y-4">
+                        <CardHeader className="text-center text-xl font-semibold text-black">Pre-Listed Students</CardHeader>
+                        {/* Divider Line */}
+                        <hr className="border-gray-300" />
 
-export default page
+                        {/* Queue */}
+                        <div className="flex flex-col gap-2">
+                            <Button className="bg-amber-100 text-amber-600 w-full border border-amber-600 hover:bg-amber-100 hover:text-amber-600">
+                                S2000
+                            </Button>
+                            {["S2001", "S2002", "S2003", "S2004", "S2005"].map((regNo, i) => (
+                                <Button
+                                    key={regNo}
+                                    className="bg-amber-100 text-amber-600 w-full border border-amber-600 hover:bg-amber-100 hover:text-amber-600"
+                                >
+                                    {regNo}
+                                </Button>
+                            ))}
+                        </div>
+                    </Card>
+                </div>
+        </div>
+    );
+}
